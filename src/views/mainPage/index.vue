fcd3a30f9ca42b3d9b55cf80b5774daae09d0673
Zoe <380933593@qq.com>
2023-10-26 13:43:56 +0800

    frame update

--- src/views/mainPage/index.vue
+++ src/views/mainPage/index.vue
@@ -7,8 +7,36 @@
     </div>
     <div class="main-body">
       <div class="chat-container">
-
       </div>
+      <el-input
+      style="width: 650px; margin-top: 20px;"
+      type="text"
+      placeholder="请输入内容"
+      v-model="textInput"
+      @change="Submit"
+      >
+        <el-button slot="append" icon="el-icon-thumb" @click="Submit" />
+      </el-input>
+
+      <transition name="fade">
+        <el-alert
+          v-if="showClosingAlert"
+          title="无效内容"
+          type="error"
+          effect="light"
+          show-icon
+        />
+      </transition>
+      <el-radio-group
+          v-model="radio"
+          @input="inputListener"
+          style="margin-top: 20px;"
+      >
+        <el-radio-button label="TEST1"></el-radio-button>
+        <el-radio-button label="TEST2"></el-radio-button>
+        <el-radio-button label="TEST3"></el-radio-button>
+        <el-radio-button label="TEST4"></el-radio-button>
+      </el-radio-group>
     </div>
   </div>
 
@@ -18,28 +46,51 @@
 export default {
   name: "index",
   data() {
-    return {}
+    return {
+      showClosingAlert: false,
+      textInput: '',
+      radio: 'TEST1'
+
+    }
   },
+
   methods: {
-    // open() {
-    //   this.$alert
-    // }
+    open() {
+      this.$alert("Zoe", "联系方式", {confirmButtonText: '确定'})
+    },
+    inputListener() {
+      // console.log(this.radio)
+    },
+    Submit() {
+      if (this.textInput === '') {
+        this.showClosingAlert = true
+        setTimeout(() => {
+          this.showClosingAlert = false
+        }, 2000)
+      }else {
+        console.log(this.textInput)
+        this.textInput = ''
+      }
+
+    }
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .head-bar {
   height: 70px;
   width: 100%;
   border-bottom: 1px solid #dcdfe6;
   display: flex;
-  .style-change {
-    right: 0px;
-  }
+  justify-content: center;
+  align-items: center;
+  column-gap: 80px;
+
 }
 .main-body {
   display: flex;
   flex-direction: column;
+  //row-gap: 20px;
   align-items: center;
   margin-top: 20px;
   .chat-container {
@@ -47,6 +98,14 @@ export default {
     width: 600px;
     border: 1px solid black;
   }
-
+  .el-alert {
+    width: 400px;
+  }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
